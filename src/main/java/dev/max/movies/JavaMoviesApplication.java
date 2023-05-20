df6863f67d3175da895e921a07a9d285bd506f5d
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-05-20 14:09:53 +0200

    setup

---
+++ src/main/java/dev/max/movies/JavaMoviesApplication.java
@@ -0,0 +1,13 @@
+package dev.max.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/max/movies/JavaMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.max.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
