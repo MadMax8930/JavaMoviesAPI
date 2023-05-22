9b7cf34b4b32ae32bdc572224189ce9c61bb418b
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-05-22 18:08:38 +0200

    home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,39 @@
-import Hero from '../hero/Hero';
+import './Home.css';
+import Carousel from 'react-material-ui-carousel';
+import { Paper } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Home = ({ movies }) => {
   return (
-    <Hero movies={movies} />
+    <div className="movie-carousel-container">
+      <Carousel>
+         {movies && movies.map((movie) => (
+            <Paper>
+               <div className="movie-card-container">
+                  <div className="movie-card" style={{"--img": `url(${movie.backdrops[0]})`}}>
+                     <div className="movie-detail">
+                        <div className="movie-poster">
+                           <img src={movie.poster} alt="poster" />
+                        </div>
+                        <div className="movie-title">
+                           <h4>{movie.title}</h4>
+                        </div>
+                        <div className="movie-buttons-container">
+                           <Link to={`/trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
+                              <div className="play-button-icon-container">
+                                 <FontAwesomeIcon className="play-button-icon" icon={faCirclePlay} />
+                              </div>
+                           </Link>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </Paper>
+         ))}
+      </Carousel>
+    </div>
   )
 }
 
