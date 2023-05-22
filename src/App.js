10f8ada05005094b126e3e1b422cc21d699f11ff
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-05-22 20:47:58 +0200

    frontend done

--- src/App.js
+++ src/App.js
@@ -7,14 +7,30 @@ import Layout from './components/Layout';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
    const [movies, setMovies] = useState();
+   const [movie, setMovie] = useState();
+   const [reviews, setReviews] = useState([]);
+
    const getMovies = async () => {
       try {
          const response = await axios.get(url + '/api/v1/movies')
-         console.log(response.data);
+         console.log('All movies:', response.data);
          setMovies(response.data);
+      } catch (err) {
+         console.log(err);
+      }
+   }
+
+   const getMovieData = async (movieId) => {
+      try {
+         const response = await axios.get(url + `/api/v1/movies/${movieId}`)
+         console.log('Selected movie:', response.data);
+         setMovie(response.data);
+         setReviews(response.data.reviewIds);
       } catch (err) {
          console.log(err);
       }
@@ -31,6 +47,13 @@ function App() {
          <Route path='/' element={<Layout/>}>
             <Route path='/' element={<Home movies={movies}/>}></Route>
             <Route path='/trailer/:ytTrailerId' element={<Trailer/>}></Route>
+            <Route path='/reviews/:movieId' 
+                   element={<Reviews getMovieData={getMovieData} 
+                                     movie={movie}
+                                     reviews={reviews}
+                                     setReviews={setReviews}/>}
+            ></Route>
+            <Route path='*' element={<NotFound/>}></Route>
          </Route>
       </Routes>
     </div>
